d99b3e3c37702e7f55f6b3e1f373c69924526513
Oscar August Heinla <ossuboy@users.noreply.github.com>
2017-05-16 14:26:30 +0300

    Regamise ja sisselogimise backend

---
+++ LogIn_Reg/login.js
@@ -0,0 +1,28 @@
+var username;
+var password;
+var a;
+var databasepassword;
+
+function login(){
+    username = document.getElementById('username').value;
+    password = document.getElementById('password').value; 
+    checkUsername();
+}
+
+function checkUsername(){
+    firebase.database().ref("userinfo").child(username).once('value', function(snapshot) {
+        if(snapshot.exists()){
+            a = "OK";
+            console.log("Kasutaja on olemas");
+            databasepassword = snapshot.val().Password;
+            if(databasepassword == password){
+                console.log("Parool õige");
+            }else{
+                console.log("Parool vale");
+            }
+        }else{
+            a = "ERROR";
+            console.log(" Kasutajat ei ole olemas");
+        }
+    }); 
+}
