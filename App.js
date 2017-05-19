bb78f9dd7e41974fb9b689fd91aa09f6dd194d19
Mariam Reintop <MReintop@users.noreply.github.com>
2017-05-19 15:06:19 +0300

    logimise dynamixs

--- App.js
+++ App.js
@@ -38,8 +38,14 @@ function saveToDB(){
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if(firebaseUser){
     console.log("You are logged in");
+		document.querySelector('.form-group').style.visibility = 'hidden';
+		document.getElementById('logout').style.visibility = 'visible';
+		document.getElementById('task').style.visibility = 'visible';
   } else {
     console.log("You are not logged in");
+		document.querySelector('.form-group').style.visibility = 'visible';
+		document.getElementById('logout').style.visibility = 'hidden';
+		document.getElementById('task').style.visibility = 'hidden';
   }
 });
 
@@ -57,9 +63,7 @@ function LogIn(){
   } else {
 	  firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
 		// IF LOG IN WORKS
-		document.querySelector('.form-group').style.visibility = 'hidden';
-		document.getElementById('logout').style.visibility = 'visible';
-		
+	
 	}).catch(function(error) {
 		// IF LOG IN DOESNT WORK
 		var errorCode = error.code;
