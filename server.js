fd0121383792c0efe4ed6d48127f5518597e850b
kertaavik <kertaavik@users.noreply.github.com>
2017-05-24 15:49:37 +0300

    Tagasi

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://kert:Eesrakendused1122@ds151951.mlab.com:51951/ea-projekt');
+mongoose.connect('mongodb://localhost/LOGIN');
 var db = mongoose.connection;
 var secure = require('./config/passport.js')
 var axios = require('axios')
