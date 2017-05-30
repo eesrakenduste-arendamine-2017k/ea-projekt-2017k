8b5ee7c05549cce46f32c95f5ab25a0a52ab945e
helemand <heleri.mandla@gmail.com>
2017-05-30 09:43:43 +0300

    mängu täiendus

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
+// node.js + express
 var fs = require("fs")
-var dataBase=JSON.parse(fs.readFileSync("dB.json", "utf8"))
-
-
-var express = require("express")
+	var express = require("express")
 	var bodyParser = require('body-parser')
-	
+	var dataBase = JSON.parse(fs.readFileSync("dB.json", "utf8"))
+
 	var app = express()
 	app.use('/', express.static('./public'))
 	app.use(bodyParser.urlencoded({
@@ -12,18 +11,23 @@ var express = require("express")
 		}))
 	app.use(bodyParser.json())
 	app.listen(3000);
-
+//andmete salvestamine dB.json faili
 app.post('/', function (req, res) {
 	res.send("test")
 	dataBase.push(req.body)
-fs.writeFileSync("dB.json", JSON.stringify(dataBase, null, 2))	
-	
+	dataBase.sort(function (a, b) {
+		return b.score - a.score;
+	});
+
+	fs.writeFileSync("dB.json", JSON.stringify(dataBase, null, 2))
+
 })
 
+//dB.json failist andmete lugemine kliendi poolele
+app.get('/top', function (req, res) {
+	console.log("request")
+	res.send(dataBase.slice(0, 10))
 
+})
 
-
-
-
-
- 
+console.log(dataBase);
