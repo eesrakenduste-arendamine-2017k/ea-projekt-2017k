f263ed332de24ba2ed2b83cba6ca22630d5f14ca
Oscar August Heinla <ossuboy@users.noreply.github.com>
2017-06-01 11:16:08 +0300

    + - FIX

--- js/gettraining.js
+++ js/gettraining.js
@@ -17,6 +17,12 @@ function getinfo(){
     username = decodeURIComponent(b[0].substring(9));
     console.log(trainingID, username);
     getTraining();
+    document.getElementById("rep1_minus").addEventListener("click", rep1Minus);
+    document.getElementById("rep2_minus").addEventListener("click", rep2Minus);
+    document.getElementById("rep3_minus").addEventListener("click", rep3Minus);
+    document.getElementById("rep1_plus").addEventListener("click", rep1Plus);
+    document.getElementById("rep2_plus").addEventListener("click", rep2Plus);
+    document.getElementById("rep3_plus").addEventListener("click", rep3Plus);
 }
 
 function getTraining(){
@@ -90,3 +96,70 @@ function getnextex(){
     //console.log(exname);
     excount += 1;
 }
+
+
+rep1Minus = function(){
+  var currentValue = parseInt(document.getElementById("rep1_x").value);
+  var newValue;
+  if (currentValue<=1) {
+    newValue= 1;
+  } else {
+    newValue = currentValue -1;
+  }
+  document.getElementById("rep1_x").value = newValue;
+};
+
+rep2Minus = function(){
+  var currentValue = parseInt(document.getElementById("rep2_x").value);
+  var newValue;
+  if (currentValue<=1) {
+    newValue= 1;
+  } else {
+    newValue = currentValue -1;
+  }
+  document.getElementById("rep2_x").value = newValue;
+};
+
+rep3Minus = function(){
+  var currentValue = parseInt(document.getElementById("rep3_x").value);
+  var newValue;
+  if (currentValue<=1) {
+    newValue= 1;
+  } else {
+    newValue = currentValue -1;
+  }
+  document.getElementById("rep3_x").value = newValue;
+};
+
+rep1Plus = function(){
+  var currentValue = parseInt(document.getElementById("rep1_x").value);
+  var newValue;
+  if (currentValue>=40) {
+    newValue= 40;
+  } else {
+    newValue = currentValue +1;
+  }
+  document.getElementById("rep1_x").value = newValue;
+};
+
+rep2Plus = function(){
+  var currentValue = parseInt(document.getElementById("rep2_x").value);
+  var newValue;
+  if (currentValue>=40) {
+    newValue= 40;
+  } else {
+    newValue = currentValue +1;
+  }
+  document.getElementById("rep2_x").value = newValue;
+};
+
+rep3Plus = function(){
+  var currentValue = parseInt(document.getElementById("rep3_x").value);
+  var newValue;
+  if (currentValue>=40) {
+    newValue= 40;
+  } else {
+    newValue = currentValue +1;
+  }
+  document.getElementById("rep3_x").value = newValue;
+};
