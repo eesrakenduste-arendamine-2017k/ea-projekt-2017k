3798e2de889fa80b4452a5d112bdd3a83ae5e319
Richard Kajaste <rkajaste@users.noreply.github.com>
2017-06-03 23:32:11 +0300

    enhanced AI

--- js/Enemy.js
+++ js/Enemy.js
@@ -8,7 +8,13 @@ function Enemy(x, y, game){
 Enemy.prototype = {
     create: function(){
         //sprites here
-        this.sprite = this.game.add.sprite(this.x, this.y, 'enemy1');
+        var random = Math.floor((Math.random() * 2) + 1);
+        if(random === 1){
+            this.sprite = this.game.add.sprite(this.x, this.y, 'enemy1');
+        } else if (random === 2){
+            this.sprite = this.game.add.sprite(this.x, this.y, 'enemy2');
+        }
+
         this.game.physics.arcade.enable(this.sprite);
         this.sprite.scale.setTo(0.25, 0.25);
         this.sprite.angle = Math.floor(Math.random()*360);
@@ -17,8 +23,17 @@ Enemy.prototype = {
 
     update: function(player){
         //collision, actions here
-        //this.game.physics.arcade.angleBetween(this.sprite, player.sprite);
-        this.game.physics.arcade.moveToObject(this.sprite, player, 200);
+        this.sprite.angle = this.game.physics.arcade.angleBetweenCenters(this.sprite, player) * (180/Math.PI) + 90;
+        if(this.game.physics.arcade.distanceBetween(this.sprite, player) < 200 ){
+            this.fire();
+            this.game.physics.arcade.moveToObject(this.sprite, player, 0);
+        } else {
+            this.game.physics.arcade.moveToObject(this.sprite, player, 50);
+        }
 
+    },
+
+    fire: function(){
+        console.log("pewpew");
     }
 };
