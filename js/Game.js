097ff6b39ca196b42cba12def6de451d2aa00f69
Ege-Rita Noor <egenoor@users.noreply.github.com>
2017-06-03 18:26:34 +0300

    added sprites and bg

--- js/Game.js
+++ js/Game.js
@@ -5,11 +5,14 @@ Main.Game = function(){};
 Main.Game.prototype = {
     create: function(){
         //sprites here
-        this.Player.create();
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.bg = this.game.add.sprite(0,0, 'bg');
+        //this.Player.create();
     },
 
     update: function(){
         //collision, actions here
-        this.Player.update();
+        //this.Player.update();
     }
 };
