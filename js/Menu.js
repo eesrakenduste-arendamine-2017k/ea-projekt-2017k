63ea8dfb9b9c6e7893399d5e819cb7cd126c99e8
Ege-Rita Noor <egenoor@users.noreply.github.com>
2017-06-04 01:55:48 +0300

    fixed buttons

--- js/Menu.js
+++ js/Menu.js
@@ -13,8 +13,8 @@ Main.Menu.prototype = {
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
         this.bg = this.game.add.sprite(0,0, 'menu');
-        this.button2 = this.game.add.button(this.game.world.centerX - 95, 300, 'btn', this.startGame, this, 1, 0, 0);
-        this.button = this.game.add.button(this.game.world.centerX - 95, 380, 'btn', this.scoreboard, this, 2, 3, 3);
+        this.button = this.game.add.button(this.game.world.centerX - 95, 300, 'btn', this.startGame, this, 2, 3, 3);
+        this.button2 = this.game.add.button(this.game.world.centerX - 95, 380, 'btn', this.scoreboard, this, 1, 0, 0);
 
     },
 
