29e8df74a542cbf838ff67fc6c7960ed19d1c24d
Ege-Rita Noor <egenoor@users.noreply.github.com>
2017-06-03 19:09:14 +0300

    added sprite on screen

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,7 @@
 var Main = Main || {};
 
-Main.Game = function(){};
+Main.Game = function(){
+};
 
 Main.Game.prototype = {
     create: function(){
@@ -8,7 +9,10 @@ Main.Game.prototype = {
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
         this.bg = this.game.add.sprite(0,0, 'bg');
-        //this.Player.create();
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.player = new Player(100, 490, this.game);
+        this.player.create();
     },
 
     update: function(){
--- js/Player.js
+++ js/Player.js
@@ -1,10 +1,21 @@
 var Main = Main || {};
 
-Main.Player = function(){};
+function Player(x, y, game){
+    this.sprite = null;
+    this.game = game;
+    this.x = x;
+    this.y = y;
+};
 
-Main.Player.prototype = {
+Player.prototype = {
     create: function(){
         //sprites here
+        this.sprite = this.game.add.sprite(this.x, this.y, "ship1");
+        this.game.physics.arcade.enable(this.sprite);
+        this.sprite.scale.setTo(0.32, 0.32);
+        this.sprite.anchor.setTo(0.5, 0.5);
+        this.sprite.angle = 90;
+        this.sprite.body.collideWorldBounds = true;
     },
 
     update: function(){
