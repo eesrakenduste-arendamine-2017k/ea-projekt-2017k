0928f19523b6c71edf3402f757f02c04f6818f45
Ege-Rita Noor <egenoor@users.noreply.github.com>
2017-06-04 15:35:21 +0300

    updated scoreboard.js

--- js/Preload.js
+++ js/Preload.js
@@ -7,7 +7,7 @@ Main.Preload.prototype = {
         //load all assets to cache and go to menu state
         this.game.load.image('bg', 'assets/bg.png');
         this.game.load.image('menu', 'assets/menu5.png');
-        this.game.load.image('score', 'assets/score1.png');
+        this.game.load.image('score', 'assets/score2.png');
         this.game.load.image('table', 'assets/table.png');
         this.game.load.spritesheet('btn', 'assets/buttons.png', 200, 100);
         this.game.load.spritesheet('btn2', 'assets/buttons1.png', 150, 100);
--- js/Scoreboard.js
+++ js/Scoreboard.js
@@ -2,6 +2,9 @@ var Main = Main || {};
 
 Main.Scoreboard = function(){
     this.button = null;
+    this.nameText = null;
+    this.scoreText = null;
+    this.timeText = null;
 };
 
 Main.Scoreboard.prototype = {
@@ -11,6 +14,7 @@ Main.Scoreboard.prototype = {
         this.scale.pageAlignVertically = true;
         this.bg = this.game.add.sprite(0,0, 'score');
         this.button = this.game.add.button(20, this.game.world.centerY + 200, 'btn2', this.redirect, this, 1, 0, 0);
+
     },
 
     update: function(){
@@ -19,6 +23,23 @@ Main.Scoreboard.prototype = {
 
     redirect: function(){
         this.game.state.start("Menu");
+    },
+    table: function() {
+        this.players = JSON.parse(localStorage.getItem('players'));
+        var top10 = this.players.sort(function(a, b) { return a.score < b.score ? 1 : -1; })
+            .slice(0, 10);
+        var i = 0;
+        top10.forEach(function(player){
+            //player.name player.score player.time
+            this.game.add.sprite(60, 60 + 60*i, 'table');
+            this.nameText = this.game.add.text(80, 60 + 60*i, player.name,
+                {fontSize: '26px', fill: '#ffffff'});
+            this.scoreText = this.game.add.text(160, 60 + 60*i, player.score,
+                {fontSize: '26px', fill: '#ffffff'});
+            this.timeText = this.game.add.text(320, 60 + 60*i, player.time,
+                {fontSize: '26px', fill: '#ffffff'});
+            i++;
+        });
     }
 
 };
