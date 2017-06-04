7bb1c711639e69dea6a2efba68b076e5677c9e08
Kristjan Liiva <krisliiv@greeny.cs.tlu.ee>
2017-06-04 15:31:29 +0300

    Small changes

--- Addition.js
+++ Addition.js
@@ -6,12 +6,10 @@ window.addEventListener("DOMContentLoaded", function(){
 		console.log("You are logged in");
 		document.querySelector('.form-group').style.visibility = 'hidden';
 		document.getElementById('logout').style.visibility = 'visible';
-		document.getElementById('tasks-view').style.visibility = 'visible';
 	} else {
 		console.log("You are not logged in");
 		document.querySelector('.form-group').style.visibility = 'visible';
 		document.getElementById('logout').style.visibility = 'hidden';
-		document.getElementById('tasks-view').style.visibility = 'hidden';
 	}
 	});
 });
--- Addition2.js
+++ Addition2.js
@@ -1,9 +1,9 @@
 window.addEventListener("DOMContentLoaded", function(){
 	firebase.auth().onAuthStateChanged(firebaseUser => {
 	if(firebaseUser){
-		document.getElementById('tasks-view').style.visibility = 'visible';
+		document.getElementById('tasks-view').style.display = 'block';
 	} else {
-		document.getElementById('tasks-view').style.visibility = 'hidden';
+		document.getElementById('tasks-view').style.display = 'none';
 	}
 	});
 });
