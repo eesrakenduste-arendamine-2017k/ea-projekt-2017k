8fc7c16b8604d07acbdaa34ea294158109d33ed3
Kristel Roosimaa <kristelroosimaa@gmail.com>
2017-06-10 14:24:07 +0300

    New approach

--- popup.js
+++ popup.js
@@ -205,103 +205,30 @@ document.addEventListener("onclick", function timer(){
 
 });
 
-/*
-  Form interaction
-*/
-var form = document.getElementById('options-form'),
-    siteListEl = document.getElementById('site-list'),
-    whitelistEl = document.getElementById('blacklist-or-whitelist'),
-    showNotificationsEl = document.getElementById('show-notifications'),
-    shouldRingEl = document.getElementById('should-ring'),
-    clickRestartsEl = document.getElementById('click-restarts'),
-    saveSuccessfulEl = document.getElementById('save-successful'),
-    timeFormatErrorEl = document.getElementById('time-format-error'),
-    background = chrome.extension.getBackgroundPage(),
-    startCallbacks = {},
-    durationEls = {};
-durationEls['work'] = document.getElementById('work-duration');
-durationEls['break'] = document.getElementById('break-duration');
+function init() {
+	document.getElementById("pomodoro").innerText = localStorage["pomodoro-selection"] || 20;
+	document.getElementById("break").innerText = localStorage["break-selection"] || 20;
 
-var TIME_REGEX = /^([0-9]+)(:([0-9]{2}))?$/;
-function readInfo (){
-    console.log("form submitted");
-    var durations = {},
-        duration, durationStr, durationMatch;
-    for (var key in durationEls) {
-        durationStr = durationEls[key].value;
-        durationMatch = durationStr.match(TIME_REGEX);
-        if (durationMatch) {
-            console.log(durationMatch);
-            durations[key] = (60 * parseInt(durationMatch[1], 10));
-            if (durationMatch[3]) {
-                durations[key] += parseInt(durationMatch[3], 10);
-            }
-        } else {
-            timeFormatErrorEl.className = 'show';
-            return false;
-        }
-    }
-    console.log(durations);
-    background.setPrefs({
-        siteList: siteListEl.value.split(/\r?\n/),
-        durations: durations,
-        showNotifications: showNotificationsEl.checked,
-        shouldRing: shouldRingEl.checked,
-        clickRestarts: clickRestartsEl.checked,
-        whitelist: whitelistEl.selectedIndex == 1
-    })
-    saveSuccessfulEl.className = 'show';
-    return false;
-}
-document.addEventListener("onfocus", function formAltered(){
-    saveSuccessfulEl.removeAttribute('class');
-    timeFormatErrorEl.removeAttribute('class');
-});
-document.addEventListener("onchange", function formAltered(){
-    saveSuccessfulEl.removeAttribute('class');
-    timeFormatErrorEl.removeAttribute('class');
-});
-/*showNotificationsEl.onchange = formAltered;
-shouldRingEl.onchange = formAltered;
-clickRestartsEl.onchange = formAltered;
-whitelistEl.onchange = formAltered; */
+	var buttonGroups = document.getElementsByClassName("time-buttons-group")
+	Array.prototype.forEach.call(buttonGroups, function(divElem) {
 
-/*Ei saa value kätte!*/
+		Array.prototype.forEach.call(divElem.childNodes, function(elem) {
+			elem.onclick = timeButtonOnClickHandler;
+		});
 
-siteListEl.value = background.PREFS.siteList.join("\n");
-showNotificationsEl.checked = background.PREFS.showNotifications;
-shouldRingEl.checked = background.PREFS.shouldRing;
-clickRestartsEl.checked = background.PREFS.clickRestarts;
-whitelistEl.selectedIndex = background.PREFS.whitelist ? 1 : 0;
-var duration, minutes, seconds;
-for (var key in durationEls) {
-    duration = background.PREFS.durations[key];
-    seconds = duration % 60;
-    minutes = (duration - seconds) / 60;
-    if (seconds >= 10) {
-        durationEls[key].value = minutes + ":" + seconds;
-    } else if (seconds > 0) {
-        durationEls[key].value = minutes + ":0" + seconds;
-    } else {
-        durationEls[key].value = minutes;
-    }
-    durationEls[key].onfocus = formAltered;
+	});
+
 }
-function setInputDisabled(state) {
-    siteListEl.disabled = state;
-    whitelistEl.disabled = state;
-    for (var key in durationEls) {
-        durationEls[key].disabled = state;
-    }
+
+/**
+ * Add click handlers to settings for user.
+ */
+function timeButtonOnClickHandler(event) {
+	var targetElem = event.target;
+	var timeSelected = +targetElem.innerText; // Get button text and convert to number
+	var settingKey = targetElem.parentNode.id; // Get id of node (tells us whether break or pomodoro)
+	localStorage[settingKey] = timeSelected; // Save in localstorage.
+	document.getElementById(settingKey.split("-")[0]).innerText = timeSelected;
 }
-startCallbacks.work = function() {
-    document.body.className = 'work';
-    setInputDisabled(true);
-}
-startCallbacks.break = function() {
-    document.body.removeAttribute('class');
-    setInputDisabled(false);
-}
-if (background.mainPomodoro.mostRecentMode == 'work') {
-    startCallbacks.work();
-}
+
+document.addEventListener('DOMContentLoaded', init);
