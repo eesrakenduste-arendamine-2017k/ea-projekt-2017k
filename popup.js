ef0551f5cd88eea55a3750433ef8b8872bc6a710
Kristel Roosimaa <kristelroosimaa@gmail.com>
2017-06-10 14:42:45 +0300

    Added timer html and js

--- popup.js
+++ popup.js
@@ -200,35 +200,3 @@ function checkPlayedSounds() {
 
 
 }
-
-document.addEventListener("onclick", function timer(){
-
-});
-
-function init() {
-	document.getElementById("pomodoro").innerText = localStorage["pomodoro-selection"] || 20;
-	document.getElementById("break").innerText = localStorage["break-selection"] || 20;
-
-	var buttonGroups = document.getElementsByClassName("time-buttons-group")
-	Array.prototype.forEach.call(buttonGroups, function(divElem) {
-
-		Array.prototype.forEach.call(divElem.childNodes, function(elem) {
-			elem.onclick = timeButtonOnClickHandler;
-		});
-
-	});
-
-}
-
-/**
- * Add click handlers to settings for user.
- */
-function timeButtonOnClickHandler(event) {
-	var targetElem = event.target;
-	var timeSelected = +targetElem.innerText; // Get button text and convert to number
-	var settingKey = targetElem.parentNode.id; // Get id of node (tells us whether break or pomodoro)
-	localStorage[settingKey] = timeSelected; // Save in localstorage.
-	document.getElementById(settingKey.split("-")[0]).innerText = timeSelected;
-}
-
-document.addEventListener('DOMContentLoaded', init);
